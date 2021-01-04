2a8e1ccd132678bfc97b87593367542ecaf66c3b
Zurkon <lukeyoshioka@gmail.com>
2021-01-04 20:51:39 -0300

    Added Player input movement with Time.deltaTime

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        float deltaY = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+        float newXPos = transform.position.x + deltaX;
+        float newYPos = transform.position.y + deltaY;
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
