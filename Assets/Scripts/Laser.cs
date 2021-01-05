559bc82339ee4d33de4d95cced59b49a4a327aed
Zurkon <lukeyoshioka@gmail.com>
2021-01-04 22:04:32 -0300

    Added Player shoot functionality and FireRate using Coroutine

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [Header("Stats")]
+    public float moveSpeed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2( 0, moveSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Movement")]
     public float moveSpeed = 10f;
+    [Tooltip("Padding for Player object's half don't go outside of MainCamera.")]
+    public Vector2 padding = new Vector2( 0.3f, 1f );
+
+    [Header("Player Stats")]
+    public GameObject laserPrefab;
+    public float fireRate = 0.1f;
+
+    Coroutine firingCoroutine;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupMoveBoundaries();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    IEnumerator FireAtWill()
+    {
+        while (true)
+        {
+            Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(fireRate);
+        }
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FireAtWill());
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(firingCoroutine);
+        }
     }
 
     private void Move()
@@ -23,9 +61,22 @@ public class Player : MonoBehaviour
         float deltaX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
         float deltaY = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
 
-        float newXPos = transform.position.x + deltaX;
-        float newYPos = transform.position.y + deltaY;
+        // Clamp Player movement to prevent him to go outside scene camera view
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetupMoveBoundaries()
+    {
+        // Get the Scene's mainCamera object
+        Camera gameCamera = Camera.main;
+
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding.x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding.x;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding.y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding.y;
     }
 }
