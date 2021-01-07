f1e64704b2980bac22034d0722cd641ba51555c9
Zurkon <lukeyoshioka@gmail.com>
2021-01-06 22:31:19 -0300

    Updated EnemySpawner to Spawn all waves from WaveConfigList and added Spawn looping functionality

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,13 +7,26 @@ public class EnemySpawner : MonoBehaviour
     [Header("Enemy Spawner Props")]
     [Tooltip("List of WaveConfig object")]
     public List<WaveConfig> waveConfigList;
-    int waveIndex = 0;
+    [Tooltip("The list position from WaveConfigList that EnemySpawner will start from.")]
+    public int startingWave = 0;
+    public bool looping = false;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
-        WaveConfig currentWave = waveConfigList[waveIndex];
-        StartCoroutine(SpawnEnemyWave(currentWave));
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
+    }
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int waveIndex = startingWave; waveIndex < waveConfigList.Count; waveIndex++)
+        {
+            WaveConfig currentWave = waveConfigList[waveIndex];
+            yield return StartCoroutine(SpawnEnemyWave(currentWave));
+        }
     }
 
     private IEnumerator SpawnEnemyWave(WaveConfig waveConfig)
@@ -22,7 +35,7 @@ public class EnemySpawner : MonoBehaviour
         {
             GameObject newEnemy = Instantiate(
                 waveConfig.enemyPrefab,
-                waveConfig.GetWaypoints()[waveIndex].transform.position,
+                waveConfig.GetWaypoints()[0].transform.position,
                 Quaternion.identity
                 );
             newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
