4f26dab2ebacd8425edb5291674a3ec1a5ad26bd
Zurkon <lukeyoshioka@gmail.com>
2021-01-08 17:51:31 -0300

    Disabled enemies when their health gets below Zero

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -22,8 +22,18 @@ public class Enemy : MonoBehaviour
     {
         DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
 
-        health -= damageDealer.GetDamage();
+        TakeDamage(damageDealer.GetDamage());
 
         damageDealer.Hit();
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
