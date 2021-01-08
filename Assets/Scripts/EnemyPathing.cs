6b4210f8ef3666dfa04a135392b7821f78b5e5ac
Zurkon <lukeyoshioka@gmail.com>
2021-01-07 21:55:01 -0300

    Updated Enemy movement using Bezier Curve functions

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -7,14 +7,27 @@ public class EnemyPathing : MonoBehaviour
     private WaveConfig waveConfig;
     private List<Transform> waypoints;
     private float moveSpeed;
-    private int waypointIndex = 0;
+
+    private float progress = 0f;
+    private float movementPerFrame = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Get waypoint positions
         waypoints = waveConfig.GetWaypoints();
+
+        // Set Enemy moveSpeed
         moveSpeed = waveConfig.moveSpeed;
-        transform.position = waypoints[waypointIndex].transform.position;
+
+        // Set Enemy initial position
+        transform.position = waypoints[0].transform.position;
+
+        // Get distance between Enemy inital position to final waypoint
+        float distance = Vector3.Distance(transform.position, waypoints[waypoints.Count - 1].transform.position);
+
+        // Set the movement of this enemy to final waypoint per frame
+        movementPerFrame = moveSpeed / distance;
     }
 
     // Update is called once per frame
@@ -28,6 +41,61 @@ public class EnemyPathing : MonoBehaviour
         waveConfig = newConfig;
     }
 
+    private void Move()
+    {
+        progress += Time.deltaTime * movementPerFrame;
+
+        Vector3 newPosition;
+
+        if (waypoints.Count == 3)
+        {
+            newPosition = QuadBezier();
+        } else
+        {
+            newPosition = LinearBezier();
+        }
+
+        transform.position = newPosition;
+
+        if (progress > 1f)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    private Vector3 LinearBezier()
+    {
+        Vector3 nextPosition = Vector3.Lerp(waypoints[0].transform.position, waypoints[1].transform.position, progress);
+        return nextPosition;
+    }
+
+    private Vector3 QuadBezier()
+    {
+        Vector3 pathAtoB = Vector3.Lerp(waypoints[0].transform.position, waypoints[1].transform.position, progress);
+        Vector3 pathBtoC = Vector3.Lerp(waypoints[1].transform.position, waypoints[2].transform.position, progress);
+        Vector3 nextPosition = Vector3.Lerp(pathAtoB, pathBtoC, progress);
+        return nextPosition;
+    }
+
+    /*
+    private Vector3 CubicBezier()
+    {
+        Vector3 pathAtoB = Vector3.Lerp(waypoints[0].transform.position, waypoints[1].transform.position, progress);
+        Vector3 pathBtoC = Vector3.Lerp(waypoints[1].transform.position, waypoints[2].transform.position, progress);
+        Vector3 pathCtoD = Vector3.Lerp(waypoints[2].transform.position, waypoints[3].transform.position, progress);
+
+        Vector3 pathABC = Vector3.Lerp(pathAtoB, pathBtoC, progress);
+        Vector3 pathBCD = Vector3.Lerp(pathBtoC, pathCtoD, progress);
+
+        Vector3 newPosition = Vector3.Lerp(pathABC, pathBCD, progress);
+
+        return newPosition;
+    }
+    */
+
+    #region Old Move Method region
+    /*
     private void Move()
     {
         if (waypointIndex < waypoints.Count)
@@ -45,4 +113,6 @@ public class EnemyPathing : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    */
+    #endregion
 }
--- Assets/Scripts/WaveConfig.cs
+++ Assets/Scripts/WaveConfig.cs
@@ -10,12 +10,13 @@ public class WaveConfig : ScriptableObject
     public GameObject pathPrefab;
     [Space]
     [Tooltip("How much time in seconds each Enemy will be spawned.")]
-    [Range(0.1f, 2f)]
+    [Range(0.1f, 1f)]
     public float timeBetweenSpawns = 0.5f;
     // public float spawnRandomFactor = 0.3f;
     [Tooltip("How many enemies will be spawned.")]
     public int numberOfEnemies = 5;
     [Tooltip("Movement Speed of each Enemy from this Wave.")]
+    [Range(1f, 6f)]
     public float moveSpeed = 2f;
 
     public List<Transform> GetWaypoints()
