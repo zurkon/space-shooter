2c50ce92a0720aa14ec49d98faa9f7fdc4798978
Zurkon <lukeyoshioka@gmail.com>
2021-01-09 18:37:58 -0300

    Added Background Music with MusicPlayer gameObject

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Awake is called before anything else
+    void Awake()
+    {
+        SetUpSingleton();
+    }
+
+    private void SetUpSingleton()
+    {
+        // GetType() gets this specific object's type (MusicPlayer)
+        if(FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
