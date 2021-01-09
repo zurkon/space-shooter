b8cf54c90d12901c881b70a89b251cb214cf2040
Zurkon <lukeyoshioka@gmail.com>
2021-01-09 16:31:28 -0300

    Added StartMenu and GameOver Scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
